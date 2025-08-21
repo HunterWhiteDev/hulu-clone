e38dd9e70cdfa248d0e43d18bb3b660bc66a511e
Hunter White <hunterwhite.dev@gmail.com>
2025-08-21 09:49:46 -0400

    rebranded from netflix to hulu

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -23,10 +23,10 @@ function Nav() {
   return (
     <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__contents">
-        <div>
+        <div className="nav__contentsLeft">
           <img
             className="nav__logo"
-            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+            src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Hulu_logo_%282018%29.svg"
             alt=""
             onClick={() => history.push("/")}
           />
